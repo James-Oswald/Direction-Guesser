62ce3c837dec4b75bcb36ab80626e120ac26e4a2
akaric108 <112095689+akaric108@users.noreply.github.com>
2024-09-27 23:47:56 -0400

    feat(draft): more erlang-y DB model

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,42 +1,121 @@
-use native_db::*;
-use once_cell::sync::Lazy;
+use std::collections::{HashMap};
+use std::path::Path;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Write;
 
-mod types;
-use types::*;
+use actix::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(MessageResponse)]
+pub struct Data {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Store {
+    dict: HashMap<String, Data>,
+}
 
-// Define the models
-// The lifetime of the models needs to be longer or equal to the lifetime of the database.
-// In many cases, it is simpler to use a static variable but it is not mandatory.
-static MODELS: Lazy<Models> = Lazy::new(|| {
-    let mut models = Models::new();
-    models.define::<Item>().unwrap();
-    models
-});
-
-fn main() -> Result<(), db_type::Error> {
-    // Create a database in memory
-    let mut db = Builder::new().create_in_memory(&MODELS)?;
-
-    // Insert data (open a read-write transaction)
-    let rw = db.rw_transaction()?;
-    rw.insert(Item { id: 1, name: "red".to_string() })?;
-    rw.insert(Item { id: 2, name: "green".to_string() })?;
-    rw.insert(Item { id: 3, name: "blue".to_string() })?;
-    rw.commit()?;
-
-    // Open a read-only transaction
-    let r = db.r_transaction()?;
-    // Retrieve data with id=3
-    let retrieve_data: Item = r.get().primary(3_u32)?.unwrap();
-    println!("data id='3': {:?}", retrieve_data);
-    // Iterate items with name starting with "red"
-    for item in r.scan().secondary::<Item>(ItemKey::name)?.start_with("red") {
-        println!("data name=\"red\": {:?}", item);
+impl Default for Store {
+    fn default() -> Store {
+        Store {
+            dict: HashMap::new(),
+        }
     }
+}
 
-    // Remove data (open a read-write transaction)
-    let rw = db.rw_transaction()?;
-    rw.remove(retrieve_data)?;
-    rw.commit()?;
-    Ok(())
+fn read_from_file<P>(path: P) -> Result<Store, Box<dyn Error>>
+where
+    P: AsRef<Path>
+{
+    let file = File::options()
+        .read(true)
+        .open(path)?;
+    let unread_json = BufReader::new(file);
+
+    let read_json = serde_json::from_reader(unread_json)?;
+
+    Ok(read_json)
+}
+
+fn unread_to_file<P>(path: P, data: Store) -> Result<Store, Box<dyn Error>>
+where
+    P: AsRef<Path>
+{
+    let mut file = File::options()
+        .write(true)
+        .read(true)
+        .create(true)
+        .open(path)?;
+    let unread_data = serde_json::to_string(&data)?;
+
+    file.write_all(unread_data.as_bytes());
+
+    Ok(data)
+}
+
+impl Actor for Store {
+    type Context = Context<Self>;
+
+    fn started(&mut self, _: &mut Context<Self>) {
+        if let Ok(recovered_store) = read_from_file("db.json") {
+            self.dict = recovered_store.dict;
+        }
+    }
+}
+
+#[derive(Message)]
+#[rtype(Data)]
+pub struct Get {
+    pub key: String,
+}
+
+impl Handler<Get> for Store {
+    type Result = Data;
+
+    fn handle(&mut self, msg: Get, _: &mut Context<Self>) -> Self::Result {
+        self.dict.get(&msg.key).unwrap().clone()
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Put {
+    pub key: String,
+    pub value: Data
+}
+
+impl Handler<Put> for Store {
+    type Result = ();
+
+    fn handle(&mut self, msg: Put, _: &mut Context<Self>) {
+        self.dict.insert(msg.key,msg.value);
+        unread_to_file("db.json", self.clone());
+    }
+}
+
+#[actix::main]
+async fn main() {
+    let addr = Store::start(Store::default());
+    // let res1 = addr.send(Put {
+    //     key: "foo".to_string(),
+    //     value: Data {
+    //         username: "abc".to_string(),
+    //         password: "123".to_string(),
+    //     },
+    // }).await;
+
+    let res = addr.send(Get {
+        key: "foo".to_string(),
+    }).await;
+
+    match res {
+        Ok(result) => println!("{result:?}"),
+        _ => println!("Communication to the actor has failed"),
+    };
+
 }
