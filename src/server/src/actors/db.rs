9e581acfa891f43296e9554742d6ba5e17425bf5
akaric108 <112095689+akaric108@users.noreply.github.com>
2024-09-29 12:58:11 -0400

    feat: generic DB implementation

---
+++ src/server/src/actors/db.rs
@@ -0,0 +1,62 @@
+use std::collections::BTreeMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::Write;
+
+use serde::Serialize;
+use tempfile::NamedTempFile;
+
+/* NOTE: this cannot be a borrowed type (i.e. ``&'a str'') because
+ * when we are undumping the DB, somebody somewhere has to own the
+ * ``str'' to begin with -ak */
+type PK = String;
+type V  = String;
+
+#[derive(Debug)]
+pub struct Actor {
+    dump_pathname: &'static str,
+    rows: BTreeMap<PK, V>
+}
+
+impl Actor {
+    pub fn new(dump_pathname: &'static str) -> Self {
+        return Actor {
+            dump_pathname,
+            rows: BTreeMap::new(),
+        };
+    }
+
+    pub fn insert(&mut self, key: impl ToString, value: impl ToString) -> Option<V> {
+        return self.rows.insert(key.to_string(), value.to_string());
+    }
+
+    pub fn get(&mut self, key: &PK) -> Option<&V> {
+        return self.rows.get(key);
+    }
+
+    pub fn dump(&self) -> Result<(), Box<dyn Error>> {
+        let mut atomic_file =
+            NamedTempFile::new()?;
+        let unread_data =
+            serde_json::to_string(&self.rows)?;
+
+        write!(atomic_file, "{}", unread_data)?;
+        atomic_file.persist(self.dump_pathname)?;
+
+        return Ok(());
+    }
+
+    pub fn undump(&mut self) -> Result<(), Box<dyn Error>> {
+        /* NOTE: we cannot use ``serde_json::from_str'' because
+         * obviously &str cannot leave it's defining scope here -ak */
+        let unread_rows =
+            BufReader::new(File::open(self.dump_pathname)?);
+        let read_rows: BTreeMap<PK, V> =
+            serde_json::from_reader(unread_rows)?;
+
+        self.rows = read_rows;
+
+        return Ok(());
+    }
+}
--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,121 +1,12 @@
-use std::collections::{HashMap};
-use std::path::Path;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::Write;
+mod actors;
+use crate::actors::db;
 
-use actix::prelude::*;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[derive(MessageResponse)]
-pub struct Data {
-    pub username: String,
-    pub password: String,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Store {
-    dict: HashMap<String, Data>,
-}
+fn main () {
+    let mut db = db::Actor::new("app.db");
 
-impl Default for Store {
-    fn default() -> Store {
-        Store {
-            dict: HashMap::new(),
-        }
-    }
-}
+    // db.insert("foo", "bar");
+    // db.dump();
+    db.undump();
 
-fn read_from_file<P>(path: P) -> Result<Store, Box<dyn Error>>
-where
-    P: AsRef<Path>
-{
-    let file = File::options()
-        .read(true)
-        .open(path)?;
-    let unread_json = BufReader::new(file);
-
-    let read_json = serde_json::from_reader(unread_json)?;
-
-    Ok(read_json)
-}
-
-fn unread_to_file<P>(path: P, data: Store) -> Result<Store, Box<dyn Error>>
-where
-    P: AsRef<Path>
-{
-    let mut file = File::options()
-        .write(true)
-        .read(true)
-        .create(true)
-        .open(path)?;
-    let unread_data = serde_json::to_string(&data)?;
-
-    file.write_all(unread_data.as_bytes());
-
-    Ok(data)
-}
-
-impl Actor for Store {
-    type Context = Context<Self>;
-
-    fn started(&mut self, _: &mut Context<Self>) {
-        if let Ok(recovered_store) = read_from_file("db.json") {
-            self.dict = recovered_store.dict;
-        }
-    }
-}
-
-#[derive(Message)]
-#[rtype(Data)]
-pub struct Get {
-    pub key: String,
-}
-
-impl Handler<Get> for Store {
-    type Result = Data;
-
-    fn handle(&mut self, msg: Get, _: &mut Context<Self>) -> Self::Result {
-        self.dict.get(&msg.key).unwrap().clone()
-    }
-}
-
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct Put {
-    pub key: String,
-    pub value: Data
-}
-
-impl Handler<Put> for Store {
-    type Result = ();
-
-    fn handle(&mut self, msg: Put, _: &mut Context<Self>) {
-        self.dict.insert(msg.key,msg.value);
-        unread_to_file("db.json", self.clone());
-    }
-}
-
-#[actix::main]
-async fn main() {
-    let addr = Store::start(Store::default());
-    // let res1 = addr.send(Put {
-    //     key: "foo".to_string(),
-    //     value: Data {
-    //         username: "abc".to_string(),
-    //         password: "123".to_string(),
-    //     },
-    // }).await;
-
-    let res = addr.send(Get {
-        key: "foo".to_string(),
-    }).await;
-
-    match res {
-        Ok(result) => println!("{result:?}"),
-        _ => println!("Communication to the actor has failed"),
-    };
-
+    println!("{:?}", db);
 }
