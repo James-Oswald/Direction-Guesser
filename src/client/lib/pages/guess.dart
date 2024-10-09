6d2fb737a78782c924d152815f2458c99f60abb7
sharie.rhea <sharierhea@gmail.com>
2024-10-09 13:21:11 -0400

    feat: add game_services to format JSON POST request for submitting a guess

--- src/client/lib/pages/guess.dart
+++ src/client/lib/pages/guess.dart
@@ -1,8 +1,13 @@
+import 'dart:ffi';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../controllers/game_services.dart';
 import '../main.dart';
 import '../widgets/permissions_card.dart';
 
@@ -16,7 +21,8 @@ class GuessPage extends StatefulWidget {
 class _GuessPageState extends State<GuessPage> {
   late CameraController controller;
   late Future<LocationPermission> locationPermissions;
-  String position = "No position.";
+  double latitude = 0.0;
+  double longitude = 0.0;
   double heading = 0.0;
 
   @override
@@ -54,19 +60,23 @@ class _GuessPageState extends State<GuessPage> {
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                   colors: [
-                    Theme.of(context).brightness == Brightness.light
+                    Theme
+                        .of(context)
+                        .brightness == Brightness.light
                         ? Color(0xFFFAF8FF)
                         : Color(0xFF121318),
-                    Theme.of(context).brightness == Brightness.light
+                    Theme
+                        .of(context)
+                        .brightness == Brightness.light
                         ? Color(0xFF495D92)
                         : Color(0xFF151B2C)
                   ]),
             ),
             child: PermissionsCard(
                 mainText:
-                    "Please enable camera permissions for Direction Guesser from your settings",
+                "Please enable camera permissions for Direction Guesser from your settings",
                 subText:
-                    "If your device does not have a camera, you are unable to play this game."));
+                "If your device does not have a camera, you are unable to play this game."));
       }));
     } else {
       return FutureBuilder<LocationPermission>(
@@ -82,19 +92,23 @@ class _GuessPageState extends State<GuessPage> {
                           begin: Alignment.topCenter,
                           end: Alignment.bottomCenter,
                           colors: [
-                            Theme.of(context).brightness == Brightness.light
+                            Theme
+                                .of(context)
+                                .brightness == Brightness.light
                                 ? Color(0xFFFAF8FF)
                                 : Color(0xFF121318),
-                            Theme.of(context).brightness == Brightness.light
+                            Theme
+                                .of(context)
+                                .brightness == Brightness.light
                                 ? Color(0xFF495D92)
                                 : Color(0xFF151B2C)
                           ]),
                     ),
                     child: PermissionsCard(
                         mainText:
-                            "Please enable location permissions for Direction Guesser from your settings",
+                        "Please enable location permissions for Direction Guesser from your settings",
                         subText:
-                            "Location permissions are needed to determine your coordinates and heading."));
+                        "Location permissions are needed to determine your coordinates and heading."));
               }));
             } else {
               return Column(children: [
@@ -108,11 +122,10 @@ class _GuessPageState extends State<GuessPage> {
                   )
                 ]),
                 FilledButton(
-                    onPressed: onGetPositionTapped,
+                    onPressed: () => submitGuess(context),
                     child: Text("Get Position")),
-                ElevatedButton(
-                    onPressed: onGetHeadingTapped, child: Text('Get Heading')),
-                Text("Current position: $position\nCurrent heading: $heading",
+                Text(
+                    "Current position: $latitude : $longitude\nCurrent heading: $heading",
                     style: TextStyle(fontSize: 12)),
                 Spacer()
               ]);
@@ -121,18 +134,44 @@ class _GuessPageState extends State<GuessPage> {
     }
   }
 
-  Future<void> onGetPositionTapped() async {
+  Future<void> submitGuess(BuildContext context) async {
     var location = await Geolocator.getCurrentPosition();
-    setState(() {
-      position = location.toString();
-    });
-  }
-
-  Future<void> onGetHeadingTapped() async {
     var direction = await FlutterCompass.events?.first;
     setState(() {
+      latitude = location.latitude;
+      longitude = location.longitude;
       heading = (direction?.heading)!;
     });
+
+    final prefs = await SharedPreferences.getInstance();
+    final sessionId = prefs.getString('session_id');
+    bool guessSentSuccessfully = await context.read<GameServices>().sendGuess(
+      // TODO: this cast should go away when we refactor to uints for session_id
+        sessionId as UnsignedInt,
+        latitude,
+        longitude,
+        heading
+    );
+
+    // Check the result of sending the guess
+    if (guessSentSuccessfully) {
+      // Show success message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Guess successful!'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    }
+    else {
+      // Show error message
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Guess unsuccessful!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
   }
 
   Future<LocationPermission> getLocationPermissions() async {
