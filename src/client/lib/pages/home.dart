3637be66c90dbd62a31f7d0a47d81e8265988d16
sharie.rhea <sharierhea@gmail.com>
2024-11-01 13:53:06 -0400

    feat: add basic leaderboard widget

--- src/client/lib/pages/home.dart
+++ src/client/lib/pages/home.dart
@@ -1,21 +1,50 @@
+import 'package:direction_guesser/widgets/leaderboard_card.dart';
 import 'package:flutter/material.dart';
 
-class homePage extends StatefulWidget {
-  const homePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<homePage> createState() => _homePageState();
+  State<HomePage> createState() => _homePageState();
 }
 
-class _homePageState extends State<homePage> {
+class _homePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      body: Container(
-        child: Text(
-          "Hello World",
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Theme.of(context).brightness == Brightness.light
+                    ? Color(0xFFFAF8FF)
+                    : Color(0xFF121318),
+                Theme.of(context).brightness == Brightness.light
+                    ? Color(0xFF495D92)
+                    : Color(0xFF151B2C)
+              ]),
         ),
-      ),
-    );
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                  LeaderboardCard(),
+                  FilledButton(
+                      onPressed: onPressedSinglePlayer,
+                      child: Text("Single-Player")),
+                  FilledButton(
+                      onPressed: onPressedMutliPlayer,
+                      child: Text("Multi-Player"))
+                ]))));
   }
+
+  // TODO:
+  void onPressedSinglePlayer() {}
+
+  // TODO:
+  void onPressedMutliPlayer() {}
 }
---
+++ src/client/lib/widgets/leaderboard_card.dart
@@ -0,0 +1,31 @@
+import 'package:direction_guesser/widgets/leaderboard_entry_pill.dart';
+import 'package:flutter/material.dart';
+
+class LeaderboardCard extends StatelessWidget {
+  const LeaderboardCard() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 6,
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    LeaderboardEntryPill(username: "username", score: 69420, rank: Rank.first),
+                    SizedBox(height: 16),
+                    LeaderboardEntryPill(username: "username", score: 69420, rank: Rank.second),
+                    SizedBox(height: 16),
+                    LeaderboardEntryPill(username: "username", score: 69420, rank: Rank.third),
+                    SizedBox(height: 16),
+                    LeaderboardEntryPill(username: "username", score: 69420, rank: Rank.other),
+                    SizedBox(height: 16),
+                    LeaderboardEntryPill(username: "username", score: 69420, rank: Rank.other)
+                  ],
+                ))));
+  }
+}
