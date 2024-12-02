64bb48bc46a891b934a37744f7ee0064c3ccbc00
akaric108 <112095689+akaric108@users.noreply.github.com>
2024-12-02 06:38:07 -0500

    fix: update frontend api calls

--- src/client/lib/controllers/game_services.dart
+++ src/client/lib/controllers/game_services.dart
@@ -5,11 +5,11 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class GameServices {
-  Future<String> randomCity() async {
+  Future<String> randomCity(double latitude, double longitude) async {
     final url = Uri.parse('http://10.0.2.2:8080/api/process');
 
     final body = jsonEncode({
-        'random_city': {}
+        'calculate_nearby': {'user_lat': latitude, 'user_lon': longitude, 'range': 20}
     });
 
     final response = await http.post(
@@ -21,20 +21,21 @@ class GameServices {
     print(response.statusCode);
     print(response.body);
 
+    /* e.g. [name: "Osterhout Cemetery", coord: {-73.80309, 42.57871}] */
     return response.body;
   }
 
-  Future<bool> sendGuess(UnsignedInt sessionId, double latitude,
-      double longitude, List<double> headings) async {
+  Future<bool> sendGuess(List<double> user_bearing, double user_lat, double user_lon, double target_lat, double target_lon) async {
     // TODO: we haven't decided on an endpoint for this yet
-    final url = Uri.parse('http://10.0.2.2:8080/game/guess');
+    final url = Uri.parse('http://10.0.2.2:8080/api/process');
 
     // create the body with all of the information needed for a guess
     final body = jsonEncode({
-      'session_id': sessionId,
-      'latitude': latitude,
-      'longitude': longitude,
-      'headings': headings
+        'user_bearing': user_bearing,
+        'user_lat': user_lat,
+        'user_lon': user_lon,
+        'target_lat': target_lat,
+        'target_lon': target_lon
     });
 
     // Send POST request to /game/guess TODO: update endpoint
@@ -51,7 +52,8 @@ class GameServices {
     // TODO: again, we haven't decided on a game API spec, this is just placeholder
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      final score = data['score'];
+      /* e.g. 93 */
+      final score = data;
 
       // Store the resulting score in shared preferences
       final prefs = await SharedPreferences.getInstance();
