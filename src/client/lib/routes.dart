7943ff75afbf8ce6e5068d7593ed836f53cf189d
sharie.rhea <sharierhea@gmail.com>
2024-12-09 19:02:41 -0500

    feat: add route transition animations

--- src/client/lib/routes.dart
+++ src/client/lib/routes.dart
@@ -5,30 +5,47 @@ import 'package:direction_guesser/pages/results.dart';
 import 'package:direction_guesser/pages/score.dart';
 import 'package:direction_guesser/pages/settings.dart';
 import 'package:flutter/material.dart';
+
 import 'pages/login.dart';
 import 'pages/register.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case '/login':
-      return MaterialPageRoute(builder: (context) => LoginPage());
+      return _createRoute(LoginPage());
     case '/register':
-      return MaterialPageRoute(builder: (context) => RegisterPage());
+      return _createRoute(RegisterPage());
     case '/home':
-      return MaterialPageRoute(builder: (context) => HomePage());
+      return _createRoute(HomePage());
     case '/guess':
-      return MaterialPageRoute(builder: (context) => GuessPage());
+      return _createRoute(GuessPage());
     case '/score':
-      return MaterialPageRoute(builder: (context) => ScorePage(city: settings.arguments.toString()));
+      return _createRoute(ScorePage(city: settings.arguments.toString()));
     case '/results':
-      return MaterialPageRoute(builder: (context) => ResultsPage());
+      return _createRoute(ResultsPage());
     case '/profile':
-      return MaterialPageRoute(builder: (context) => ProfilePage());
+      return _createRoute(ProfilePage());
     case '/settings':
-      return MaterialPageRoute(builder: (context) => SettingsPage());
+      return _createRoute(SettingsPage());
     default:
-      return MaterialPageRoute(
-          builder: (context) =>
-              ErrorWidget(Exception('Default route reached!')));
+      return _createRoute(LoginPage());
   }
+}
+
+Route _createRoute(Widget destination) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => destination,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
