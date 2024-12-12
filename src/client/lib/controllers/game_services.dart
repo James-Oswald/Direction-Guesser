12dd0f9a9861d1b118c6d462178479a1d7eb6552
10dine <ishtyaqkhan1@gmail.com>
2024-12-12 11:08:50 -0500

    small refactor: server URL references in game and user services

--- src/client/lib/controllers/game_services.dart
+++ src/client/lib/controllers/game_services.dart
@@ -6,12 +6,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class GameServices {
 
-  String deviceUrl = 'http://localhost:8080';
-  String emulatorUrl = 'http://10.0.2.2:8080';
-  String productionUrl = 'http://dirg.ieeeualbany.org';
+  String serverUrl = 'http://localhost:8080';
+  //String serverUrl = 'http://10.0.2.2:8080';
+  //String serverUrl = 'http://dirg.ieeeualbany.org';
 
   Future<String> randomCity(double latitude, double longitude) async {
-    final url = Uri.parse('$deviceUrl/api/process');
+    final url = Uri.parse('$serverUrl/api/process');
 
     final body = jsonEncode({
         'calculate_nearby': {'user_lat': '$longitude', 'user_lon': '$latitude', 'range': 20}
@@ -28,7 +28,7 @@ class GameServices {
   }
 
   Future<List<Map<String, dynamic>>> getRandomCities(String latitude, String longitude, int amount) async {
-    final url = Uri.parse('$deviceUrl/api/process');
+    final url = Uri.parse('$serverUrl/api/process');
 
     //latitude and longitude are reversed in the API
     final body = jsonEncode({
@@ -57,7 +57,7 @@ class GameServices {
 
   Future<bool> sendGuess(List<double> user_bearing, double user_lat, double user_lon, double target_lat, double target_lon) async {
     // TODO: we haven't decided on an endpoint for this yet
-    final url = Uri.parse('$deviceUrl/game/guess');
+    final url = Uri.parse('$serverUrl/game/guess');
 
     // create the body with all of the information needed for a guess
     final body = jsonEncode({
